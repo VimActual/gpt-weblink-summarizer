0842b1c61ab8bc37c97daad00ccaf884ea6e6ff6
Vim <121538257+VimActual@users.noreply.github.com>
2023-01-19 15:06:37 -0600

    Update computer-explain.py

--- computer-explain.py
+++ computer-explain.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 from threading import Thread
+import requests
+import os
 import yt_dlp
 import re
 
@@ -13,10 +15,14 @@ class GetSub():
         self.remove_non_speach()
 
     def getsub(self):
-        thread = Thread(target=yt_dlp.main, args=(["--write-auto-sub", "--convert-subs=vtt", "--skip-download", self.video_url, "-o", self.videoname],))
+        #thread = Thread(target=yt_dlp.main, args=(["--write-auto-sub", "--convert-subs=vtt", "--skip-download", self.video_url, "-o", self.videoname],))
+        thread = Thread(target=yt_dlp.main, args=(["--write-auto-sub", "--sub-langs=en.*", "--skip-download", self.video_url, "-o", self.videoname],))
         thread.start()
         thread.join()
-        self.file_name = self.videoname + '.en.vtt'
+        if os.path.exists(self.videoname + '.en-en.vtt'):
+            self.file_name = self.videoname + '.en-en.vtt'
+        else:
+            self.file_name = self.videoname + '.en.vtt'
 
     def remove_non_speach(self):
         pattern = re.compile(r'^.*(\d{2}:\d{2}:\d{2}\.\d{3}).*$')
@@ -33,6 +39,30 @@ class GetSub():
                     line_cleaned = line.replace('\n', ' ')
                     file.write(line_cleaned)
 
+class AskGPT():
+    def __init__(self):
+        return 0
+    def ask(self, question, data):
+        r = requests.post('https://api.genesistranslation.com/v1/gtp3',
+                json={"user_key": "<your key>",
+                    "query": question
+                    })
+
+        data = r.json()
+        print(data['result'])
 if __name__ == '__main__':
     g = GetSub()
     g.sub_only()
+
+#financial
+"""
+1. Give me a breif summary of the video
+2. provide me as many bullet points as you can about the video
+3. Does the speaker(s) make any forecasts
+"""
+#scientific
+"""
+1. Give me a beif summary
+2. Does this contradict any main stream scientific thought?
+3. Give me as many bullet points as you can about the video?
+"""
