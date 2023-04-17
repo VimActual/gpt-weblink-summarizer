f9068a06935c738f6a8e0f3f1497b567344dcdf1
VimActual <vim@vimsbasecamp.town>
2023-04-17 20:30:58 +0000

    Working

--- computer-explain.py
+++ computer-explain.py
@@ -1,39 +1,28 @@
-#!/usr/bin/env python
-from threading import Thread
-import requests
-import os
 import re
+import requests
+import urllib.parse
+import openai
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 import yt_dlp
-from chatgpt import Conversation
+from threading import Thread
+import os
 
-def main():
-    vid1 = GetSub()
-    text1 = vid1.get_text()
-    question_dict = {}
-    question_dict['brief_summary'] = 'Give me a brief summary of this video\n'
-    #question_dict['answer_to_title'] = f'the title of this video is "{video_title}" what was that discovery?\n'
-    chatgpt = AskChatGPT()
-    query = question_dict['brief_summary'] + text1
-    #summation = chatgpt.ask(query)
-    #print(summation)
-    #summation = vid1.ask(quesitons, text1)
-    # list engines
-    #engines = openapi.Engine.list()
-    # print the first engine's id
-    #print(engines.data[0].id)
+# Set OpenAI API key
+openai.api_key = "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"
 
 class GetSub():
-    def __init__(self):
-        self.video_url = input("Enter video-url: ")
-        self.videoname = input("Enter name for video: ")
+    def __init__(self, video_url, videoname):
+        self.video_url = video_url
+        self.videoname = videoname
         self.get_raw_sub()
         self.remove_non_speach()
-        
+
     def get_text(self):
         return self.text
 
     def get_raw_sub(self):
-        #thread = Thread(target=yt_dlp.main, args=(["--write-auto-sub", "--sub-langs=en.*", "--skip-download", self.video_url, "-o", self.videoname],))
         thread = Thread(target=yt_dlp.main, args=(["--write-auto-sub", "--skip-download", self.video_url, "-o", self.videoname],))
         thread.start()
         thread.join()
@@ -46,13 +35,11 @@ class GetSub():
         pattern = re.compile(r'^.*(\d{2}:\d{2}:\d{2}\.\d{3}).*$')
         self.text = ''
         with open(self.file_name, 'r') as file:
-            print(f'opening file for reading {self.file_name}')
             lines = file.readlines()
         with open(self.file_name, 'w') as file:
             last_line = ''
             for line in lines:
                 if not pattern.match(line) and line != last_line and line != ' \n' and line != '\n':
-                    #print(f'line: {repr(line)}\nvsus: {repr(last_line)}')
                     last_line = line
                     line_cleaned = line.rstrip()
                     line_cleaned = line.replace('\n', ' ')
@@ -60,37 +47,57 @@ class GetSub():
                     self.text = self.text + line_cleaned
         return self.text
 
-class AskChatGPT():
-    def ask(self, query):
-        converstation = Conversation(config_path='')
-        for chunk in converstation.stream(query):
-            print(chunk, end="")
+def get_website_text_selenium(url):
+    options = Options()
+    options.headless = True
+    driver = webdriver.Firefox(options=options)
+    
+    try:
+        driver.get(url)
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-class AskGPT3():
-    def __init__(self, openapi_key):
-        self.openapi_key = openapi_key
+        for script in soup(["script", "style"]):
+            script.extract()
 
-    def ask(self, question, text):
-        question = question + '\n' + text
-        r = requests.post('https://api.genesistranslation.com/v1/gtp3',
-                json={"user_key": self.openapi_key,
-                    "query": question
-                    })
-        data = r.json()
-        print(data['result'])
+        text = soup.get_text()
+        lines = (line.strip() for line in text.splitlines())
+        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+        text = '\n'.join(chunk for chunk in chunks if chunk)
 
-if __name__ == '__main__':
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        text = None
+
+    driver.quit()
+    return text
+
+def ask_openai(question, text):
+    prompt = f"Question: {question}\nText: {text}\nAnswer:"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    return response.choices[0].text.strip()
+
+def main():
+    question = input("Enter a question: ")
+    url = input("Enter a URL: ")
+
+    if "youtube.com" in url:
+        videoname = "video_subtitles"
+        get_sub_instance = GetSub(url, videoname)
+        text = get_sub_instance.get_text()
+    else:
+        text = get_website_text_selenium(url)
+    if text:
+        answer = ask_openai(question, text)
+        print(answer)
+    else:
+        print("Failed to fetch text content. Please try another URL.")
+
+if __name__ == "__main__":
     main()
-
-#financial
-"""
-1. Give me a breif summary of the video
-2. provide me as many bullet points as you can about the video
-3. Does the speaker(s) make any forecasts
-"""
-#scientific
-"""
-1. Give me a beif summary
-2. Does this contradict any main stream scientific thought?
-3. Give me as many bullet points as you can about the video?
-"""
